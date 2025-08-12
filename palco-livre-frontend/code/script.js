b1867e61a6529f2f1838003a9b0b2245a29eb79c
wasddsawd <rafaelkrugervidaldasilva@gmail.com>
2025-08-12 16:07:03 -0300

    Add "script.js"

---
+++ palco-livre-frontend/code/script.js
@@ -0,0 +1,8 @@
+function submitCriar() {
+  const inputnomeC = document.getElementById("nomeconta");
+  const inputEmailC = document.getElementById("emailconta");
+  const inputSenhaC = document.getElementById("senhaconta");
+  const valueNomeC = inputnomeC.value;
+  const valueEmailC = inputEmailC.value;
+  const valueSenhaC = inputSenhaC.value;
+}
