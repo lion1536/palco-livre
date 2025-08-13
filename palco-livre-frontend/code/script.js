a5842893499c68fd1438b3b5318ff955f17eebb7
wasddsawd <rafaelkrugervidaldasilva@gmail.com>
2025-08-13 14:32:16 -0300

    Add "pfp","login","cadastro"

--- palco-livre-frontend/code/script.js
+++
@@ -1,8 +0,0 @@
-function submitCriar() {
-  const inputnomeC = document.getElementById("nomeconta");
-  const inputEmailC = document.getElementById("emailconta");
-  const inputSenhaC = document.getElementById("senhaconta");
-  const valueNomeC = inputnomeC.value;
-  const valueEmailC = inputEmailC.value;
-  const valueSenhaC = inputSenhaC.value;
-}
