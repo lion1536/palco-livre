bf5116d342ca7e35af370b9ae21479a13344f7e4
lion1536 <pedroarmiliato2006@gmail.com>
2025-08-13 14:25:55 -0300

    Login route complete

---
+++ palco-livre-frontend/code/login.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btnLogin = document.querySelector(".submit");
+
+  btnLogin.addEventListener("click", async () => {
+    const email = document.getElementById("email").value.trim();
+    const senha = document.getElementById("senha").value.trim();
+
+    if (!email || !senha) {
+      alert("Todos os campos são obrigatórios.");
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, senha }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert("Login realizado com sucesso!");
+        console.log("Usuário logado:", data.usuario);
+        console.log("Token JWT:", data.token);
+      } else {
+        alert("Erro no login:" + (data.error || "Erro desconhecido"));
+      }
+    } catch (error) {
+      alert("Erro na conexão com o servidor: " + error.message);
+    }
+  });
+});
